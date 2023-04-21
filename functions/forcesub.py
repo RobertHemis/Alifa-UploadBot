c6af829ba273dbbf4ce2b134cd6c0a574a1c7a16
Robert Hemis <130218888+RobertHemis@users.noreply.github.com>
2023-04-21 23:45:05 +0600

    Add files via upload

---
+++ functions/forcesub.py
@@ -0,0 +1,49 @@
+import asyncio
+from plugins.config import Config
+from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+async def handle_force_subscribe(bot, message):
+    try:
+        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return 400
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=message.from_user.id,
+                text="Banned.",
+                parse_mode="markdown",
+                disable_web_page_preview=True,
+                reply_to_message_id=message.message_id,
+            )
+            return 400
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="Join to use.",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Channel URL", url=invite_link.invite_link)
+                    ]
+                ]
+            ),
+            parse_mode="markdown",
+            reply_to_message_id=message.message_id,
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="Something went wrong. [Developer](https://t.me/abeseil).",
+            parse_mode="markdown",
+            disable_web_page_preview=True,
+            reply_to_message_id=message.message_id,
+        )
+        return 400
+
+
